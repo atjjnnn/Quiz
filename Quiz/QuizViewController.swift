adb1f7f5246a9ff5091e8a2ca0d40bfdea10843a
atjjnnn <atjjnnn@gmail.com>
2024-10-08 00:34:34 +0900

    update app

--- Quiz/QuizViewController.swift
+++ Quiz/QuizViewController.swift
@@ -33,11 +33,55 @@ class QuizViewController: UIViewController {
         self.quizCard.quizImageView.image = UIImage(named: manager.currentQuiz.imageName)
     }
     
+    func answer() {
+        // 移動するCGAffineTransformオブジェクト
+        var translationTransform: CGAffineTransform
+        // X軸方向の移動距離
+        let screenWidth = UIScreen.main.bounds.width
+        // Y軸方向の移動距離
+        let y = UIScreen.main.bounds.height * 0.2
+        
+        // 回答によってtranslationTransformの内容を変える
+        if self.quizCard.style == .right {
+            // ○回答の時は右へ移動
+            translationTransform = CGAffineTransform(translationX: screenWidth, y: y)
+            self.manager.answerQuiz(answer: true)
+        } else {
+            // ×回答の時は左へ移動
+            translationTransform = CGAffineTransform(translationX: -screenWidth, y: y)
+            self.manager.answerQuiz(answer: false)
+        }
+        
+        // クイズのカードをアニメーションさせて移動する
+        // 0.1秒遅延させて0.5秒でカードを移動する
+        UIView.animate(withDuration: 0.5, delay: 0.1, options: [.curveLinear], animations: {
+            // クイズのカードのtransformプロパティにtranslationTransformを設定
+            self.quizCard.transform = translationTransform
+        }, completion: { [unowned self] (finished) in
+            if finished {
+                self.showNextQuiz()
+            }
+        })
+    }
+    
+    func showNextQuiz() {
+        // 次のクイズを取得
+        self.manager.nextQuiz()
+        // transformプロパティに加えられた変更をリセットし、
+        // クイズのカードをもとの位置に
+        self.quizCard.transform = CGAffineTransform.identity
+        // カードの状態を初期状態に
+        self.quizCard.style = .initial
+        // クイズを表示
+        self.loadQuiz()
+    }
+    
     @objc func dragQuizCard(_ sender: UIPanGestureRecognizer) {
         switch sender.state {
         case .began, .changed:
             self.transformQuizCard(gesture: sender)
         case .ended:
+            self.answer()
             break
         default:
             break
